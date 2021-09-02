0693f9927ab0693d3baf7d1486dc570a15f78937
Washington Alexandre da Silva <was.alexandre42@gmail.com>
2021-09-01 21:49:56 -0300

    feature: add states loading

--- lib/screens/contacts_list.dart
+++ lib/screens/contacts_list.dart
@@ -1,3 +1,5 @@
+import 'package:fluter_persistencia/database/app_database.dart';
+import 'package:fluter_persistencia/models/contact.dart';
 import 'package:fluter_persistencia/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
@@ -8,30 +10,72 @@ class ContactsList extends StatelessWidget {
         appBar: AppBar(
           title: Text('Contatos'),
         ),
-        body: ListView(
-          children: <Widget>[
-            Card(
-              child: ListTile(
-                title: Text(
-                  'Was',
-                  style: TextStyle(fontSize: 24),
-                ),
-                subtitle: Text(
-                  '1000',
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-            )
-          ],
+        body: FutureBuilder(
+          future:
+              Future.delayed(Duration(seconds: 1)).then((value) => findAll()),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                break;
+              case ConnectionState.waiting:
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircularProgressIndicator(),
+                      Text('Loading'),
+                    ],
+                  ),
+                );
+              case ConnectionState.active:
+                break;
+              case ConnectionState.done:
+                final List<Contact> contacts = snapshot.data as List<Contact>;
+                return ListView.builder(
+                  itemBuilder: (context, index) {
+                    final Contact contact = contacts[index];
+                    return _ContactItem(contact);
+                  },
+                  itemCount: contacts.length,
+                );
+            }
+            return Text('unknoun error');
+          },
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
             Navigator.of(context)
-                .push(
-                  MaterialPageRoute(builder: (context) => ContactForm(),)
-                ).then((newContact) => debugPrint(newContact.toString()),);
+                .push(MaterialPageRoute(
+                  builder: (context) => ContactForm(),
+                ))
+                .then(
+                  (newContact) => debugPrint(newContact.toString()),
+                );
           },
         ));
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(fontSize: 24),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(fontSize: 16),
+        ),
+      ),
+    );
   }
 }
