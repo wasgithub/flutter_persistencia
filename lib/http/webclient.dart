a1485c76b5f657c8fcd59be66588e1eebbaff987
Washington Alexandre da Silva <was.alexandre42@gmail.com>
2021-09-05 19:11:28 -0300

    feature: add interceptor request/ response

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,8 +1,27 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
 
 const String urlTransactions = 'http://192.168.0.24:8080/transactions';
 
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    print("passou aqui");
+    print("statusCode: ${data.statusCode}");
+    print(data.toString());
+    return data;
+  }
+}
+
 void findAll() async {
-  final Response response = await get(Uri.parse(urlTransactions));
+  final Client client = InterceptedClient.build(
+    interceptors: [LoggingInterceptor()],
+  );
+  final Response response = await client.get(Uri.parse(urlTransactions));
   print(response.body);
 }
